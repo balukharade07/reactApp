204f179af5a3ce3bf22ab110e6c7fd6d80287cbd
Balu Kharade <kharadeb999@gmail.com>
2019-04-09 23:33:26 +0530

    input element

--- src/Component_tab/Home.js
+++ src/Component_tab/Home.js
@@ -1,41 +1,95 @@
-import React, { Component } from 'react';
-import Demo from './demo'; 
-import Button from '@material-ui/core/Button';
+import React, { Component } from "react";
+import Demo from "./demo";
+import Button from "@material-ui/core/Button";
 
 export default class Home extends Component {
-
-  constructor (props){
+  constructor(props) {
     super(props);
-        this.state= {
-          Username:'',
-        }
-}
+    this.state = {
+      username: "",
+      password: "",
+      age: "",
+      email: ""
+    };
+  }
 
-onChange = (e) => {
-    this.setState({[e.target.name] : e.target.value});
-}
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-onSubmitContact = (e) =>{
-  e.preventDefault();
+  onSubmitContact = e => {
+    e.preventDefault();
     console.log(this.state);
-}
+    this.setState({
+      username: "",
+      password: "",
+      age: "",
+      email: ""
+    });
+  };
   render() {
+    const formElements = [
+      {
+        label: "Username",
+        type: "text",
+        autoComplete: "OFF",
+        className: "form-control",
+        placeholder: "Username",
+        name: "username",
+        value: this.state.username,
+        required: true
+      },
+      {
+        label: "Age",
+        type: "number",
+        autoComplete: "OFF",
+        className: "form-control",
+        placeholder: "Age",
+        name: "age",
+        value: this.state.age,
+        required: true
+      },
+      {
+        label: "Email",
+        type: "email",
+        autoComplete: "OFF",
+        className: "form-control",
+        placeholder: "Email Address",
+        name: "email",
+        value: this.state.email,
+        required: true
+      },
+      {
+        label: "Password",
+        type: "password",
+        autoComplete: "OFF",
+        className: "form-control",
+        placeholder: "Password",
+        name: "password",
+        value: this.state.password,
+        required: true
+      }
+    ];
 
     return (
       <div>
         <h2>Homw is work</h2>
 
         <form onSubmit={this.onSubmitContact}>
-                    <div className="form-group">
-                        <label>Name</label>
-                        <Demo  name="Uservalue" place='User Name'/>
-                    </div>
-                    <div className="text-center">
-                            <Button type="submit" className="col-lg-4 mt-5" id="primary" variant="contained" color='primary'> Submit </Button>
-                    </div>
-                </form>
-       
+          <Demo formElements={formElements} onChange={this.onChange} />
+          <div className="text-center">
+            <Button
+              type="submit"
+              className="col-lg-4 mt-5"
+              id="primary"
+              variant="contained"
+              color="primary"
+            >
+              Submit
+            </Button>
+          </div>
+        </form>
       </div>
-    )
+    );
   }
 }
--- src/Component_tab/demo.js
+++ src/Component_tab/demo.js
@@ -1,34 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import Button from '@material-ui/core/Button';
 
 export default class demo extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-   
-    }
-  }
- 
-
   render() {
-
-      const { Username , place ,Uservalue } = this.props;
+    const { formElements, onChange } = this.props;
 
     return (
-        <div>
-           
-                        <input 
-                            type="text"
-                            autoComplete="OFF"
-                            className="form-control" 
-                            placeholder={place}
-                            onChange={this.onChange}
-                            name={Username}
-                            value={Uservalue}
-                            required
-                         />
-                   
-        </div>
-    )
+      <div>
+        {formElements &&
+          formElements.map((value, index) => (
+            <div className="form-group" key={index}>
+              <label>{value.label}</label>
+              <input
+                type={value.type}
+                autoComplete={value.autoComplete}
+                className={value.className}
+                placeholder={value.placeholder}
+                name={value.name}
+                value={value.value}
+                required={value.required}
+                onChange={onChange}
+              />
+            </div>
+          ))}
+      </div>
+      // <div>
+
+      //                 <input
+      //                     type="text"
+      //                     autoComplete="OFF"
+      //                     className="form-control"
+      //                     placeholder={place}
+      //                     onChange={this.onChange}
+      //                     name={Username}
+      //                     value={Uservalue}
+      //                     required
+      //                  />
+
+      // </div>
+    );
   }
 }
